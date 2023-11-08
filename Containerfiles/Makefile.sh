12725cc13f43f31e3fdcb615635451475eafbb4f
manfredmuth <mmuth@redhat.com>
2023-11-08 18:45:25 +0100

    Build worked and pics are added but with gitignore file

---
+++ Containerfiles/Makefile.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/bash
+
+# Variables
+
+# The local directory we want to share in the container
+# this directory contains our images
+local_dir=/home/mmuth/GIT/opencv/Containerfiles/openCV/cascade
+# The directory in our container where $local_dir will be mounted
+target_dir=/cascade
+
+# Check for the right Containerfile based on C_ID value
+
+grep C_ID_openCV Containerfile >> $0
+exit_code=`echo $?`
+if [ $exit_code != 0 ];then
+  echo "Not the right Containerfile found"
+  echo "Reason might be missing file or wrong identifier C_ID"
+  exit 1
+fi
+
+echo "Containerfile found - we start"
+
+# Show the variables which are set
+echo "---------------------"
+echo "local_dir" $local_dir
+echo "target_dir" $target_dir
+
+# Clean up old stuff and remove old images
+echo "---------------------"
+echo "INFO - Clean up - remove old images"
+for i in `podman image ls -a |grep ^"<none>" |awk '{print $3}'`
+  do
+  podman image rm $i
+done
+
+# Create the container image
+echo "---------------------"
+echo "INFO - create the container image"
+podman build -t opencv .
+
+# Set proper rights for the mount
+echo "---------------------"
+echo "INFO - Set proper local rights for the mount"
+echo "podman unshare chown -R 0:0 " $local_dir
+echo ""
+# podman unshare chown -R 0:0 /home/mmuth/GIT/opencv/Containerfiles/openCV/cascade```
+
+#podman run --rm -d -t \
+#  --name openCV \
+#  -v /home/mmuth/GIT/cat_prey_project/Containerfiles/openCV/cascade:/cascade:Z \
+#  cat/opencv
+
+
+
+
+########
+
+#podman attach openCV
+
+#exit with CTRL-P,CTRL-Q otherwise the container will stop
+#This refers to [1]
+
+## Resources
+# [1] [Troubelshoot podman terminates container on exit](https://github.com/containers/libpod/blob/master/troubleshooting.md)
+########
+
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV
+# C_ID_openCV - Identifier if we have the right file
+# C_ID_openCV - Identifier if we have the right file
+# C_ID_openCV - Identifier if we have the right file
+# C_ID_openCV - Identifier if we have the right file
+# C_ID_openCV - Identifier if we have the right file
